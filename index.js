a6e6ec6659b6c5cbe67eaf6354fd40eb8de5b0d4
casperwang <casperwang2014@gmail.com>
2018-07-02 10:59:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var user = card[ran];
 draw(1); // show the card
 card[ran] = 0;
 while (card[ran] == 0) {
-  ran = Math.floor(Math.random()*16);
+	ran = Math.floor(Math.random()*16);
 }
 var opponent = card[ran];
 card[ran] = 0;
@@ -18,96 +18,96 @@ var i;
 var nowCard;
 var protect = 0;
 for (i = 2; i <= 14 && finsih == false; i++) {
-  while (card[ran] == 0) {
-    ran = Math.floor(Math.random()*16);
-  }
-  nowCard = card[ran];
-  card[ran] = 0;
-  now = (now+1)%2; //next player
-  if (now == 0) {
-    if () {
-      nowCard = user;
-    }
-    usingCard(nowCard, 0);
-  } else {
-    if () {
-      nowCard = opponent;
-    }
-    usingCard(nowCard, 0);
-  }
+	while (card[ran] == 0) {
+		ran = Math.floor(Math.random()*16);
+	}
+	nowCard = card[ran];
+	card[ran] = 0;
+	now = (now+1)%2; //next player
+	if (now == 0) {
+		if () {
+			nowCard = user;
+		}
+		usingCard(nowCard, 0);
+	} else {
+		if () {
+			nowCard = opponent;
+		}
+		usingCard(nowCard, 0);
+	}
 }
 function usingCard(n, now) {
-  if (n == 1 && protect == 0) {
-    var guess = ;
-    if (now == 0) {
-      if (guess == opponent) {
-        finish = true;
-        winner = 0;
-        return;
-      } else {
-        return;
-      }
-    } else {
-      if (guess == user) {
-        finish = true;
-        winner = 1;
-        return;
-      } else {
-        return;
-      }
-    }
-  } else if (n == 2 && protect == 0) {
-    show()
-  } else if (n == 3 && protect == 0) {
-    if (user > opponent) {
-      finish = true;
-      winner = 0;
-      return;
-    } else if (user == opponent) {
-      return;
-    } else {
-      finish = true;
-      winner = 1;
-      return;
-    }
-  } else if (n == 4) {
-    protect = 1;
-    return;
-  } else if (n == 5) {
-    while (card[ran] == 0) {
-      ran = Math.floor(Math.random()*16);
-    }
-    nowCard = card[ran];
-    card[ran] = 0;
-    i++;
-    if (now == 0) {
-      opponent = nowCard;
-    } else {
-      user = nowCard;
-    }
-    return;
-  } else if (n == 6 && protect == 0) {
-    var temp = user;
-    user = opponent;
-    opponent = user;
-    return;
-  } else if (n == 7) {
-    return;
-  } else {
-    finish = true;
-    winner = (now+1)%2;
-    return;
-  }
-  if (protect > 0) {
-    protect = (protect+1)%3;
-  }
+	if (n == 1 && protect == 0) {
+		var guess = ;
+		if (now == 0) {
+			if (guess == opponent) {
+				finish = true;
+				winner = 0;
+				return;
+			} else {
+				return;
+			}
+		} else {
+			if (guess == user) {
+				finish = true;
+				winner = 1;
+				return;
+			} else {
+				return;
+			}
+		}
+	} else if (n == 2 && protect == 0) {
+		show()
+	} else if (n == 3 && protect == 0) {
+		if (user > opponent) {
+			finish = true;
+			winner = 0;
+			return;
+		} else if (user == opponent) {
+			return;
+		} else {
+			finish = true;
+			winner = 1;
+			return;
+		}
+	} else if (n == 4) {
+		protect = 1;
+		return;
+	} else if (n == 5) {
+		while (card[ran] == 0) {
+			ran = Math.floor(Math.random()*16);
+		}
+		nowCard = card[ran];
+		card[ran] = 0;
+		i++;
+		if (now == 0) {
+			opponent = nowCard;
+		} else {
+			user = nowCard;
+		}
+		return;
+	} else if (n == 6 && protect == 0) {
+		var temp = user;
+		user = opponent;
+		opponent = user;
+		return;
+	} else if (n == 7) {
+		return;
+	} else {
+		finish = true;
+		winner = (now+1)%2;
+		return;
+	}
+	if (protect > 0) {
+		protect = (protect+1)%3;
+	}
 }
 
 function draw(x) {
-  if(x==1) {
-    document.getElementById("card1").src = "./card/"+user+".jpg";
-  } else if(x==2) {
-    document.getElementById("card2").src = "./card/"+user+".jpg";
-  }
+	if(x==1) {
+		document.getElementById("card1").src = "./card/"+user+".jpg";
+	} else if(x==2) {
+		document.getElementById("card2").src = "./card/"+user+".jpg";
+	}
 }
 
