251a034f1cf04cd85faa4b56bb0d516313b38ff5
casperwang <casperwang2014@gmail.com>
2018-07-02 09:50:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 document.title = "Love Letter";
+var card = [1,1,1,1,1,2,2,3,3,4,4,5,5,6,7,8];
+var ran = Math.floor(Math.random()*16);
+var user = card[ran];
+card[ran] = 0;
+while (card[ran] == 0) {
+  ran = Math.floor(Math.random()*16);
+}
+var opponent = card[ran];
+card[ran] = 0;
+var first = Math.floor(Math.random()*2);
+if ()
